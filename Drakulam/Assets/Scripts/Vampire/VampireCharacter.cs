4734870205809c4a3200d342924f5d21ada6f0db
Viktor <viktor.gerasimenko.2014@mail.ru>
2021-02-25 16:28:42 +0300

    #9_create_vampire_model
    - idle animation
    - walk animation
    - attack animation
    - hurt animation
    - death animation

---
+++ Drakulam/Assets/Scripts/Vampire/VampireCharacter.cs
@@ -0,0 +1,14 @@
+
+class VampireCharacter : Character
+{
+
+    public VampireCharacter()
+    {
+        maxSpeed = 15f;
+        health = 70;
+        viewRadius = 8;
+        attackDamage = 5;
+    }
+
+
+}
