d8a19cefe7b98e19d2375e53405e3f774d0f0c7f
Aleksei <alex.zedfeelgood@gmail.com>
2021-03-06 15:47:45 +0300

    rewrite using input system

--- Drakulam/Assets/Scripts/Character.cs
+++ Drakulam/Assets/Scripts/Character.cs
@@ -1,8 +1,11 @@
+using System;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class Character : MonoBehaviour
 {
+    [SerializeField] private CharacterInput controller;
     //direction
     protected bool isFacingRight = true;
 
@@ -22,10 +25,51 @@ public class Character : MonoBehaviour
     protected float attackDamage;
 
 
+    private void OnEnable()
+    {
+        controller.Enable();
+    }
 
-    public Character() {}
+    private void OnDisable()
+    {
+        controller.Disable();
+    }
 
+    private void Awake()
+    {
+        controller = new CharacterInput();
+        controller.Player.Atack.performed += ctx => Attack();
+        controller.Player.Die.performed += ctx => { anim.SetTrigger("death"); };
+        controller.Player.Hurt.performed += ctx => { anim.SetTrigger("hurt"); };
+    }
 
+    private void Move(Vector2 direction)
+    {
+        float moveX = direction.x;
+        float moveY = direction.y;
+        body2D.velocity = new Vector2(moveX * maxSpeed, moveY * maxSpeed);
+        if (moveX > 0 && !isFacingRight)
+            Flip();
+        else if (moveX < 0 && isFacingRight)
+            Flip();
+        
+        if (Mathf.Abs(moveX) > Mathf.Epsilon || Mathf.Abs(moveY) > Mathf.Epsilon) {
+            // Reset timer
+            delayToIdle = 0.05f;
+            anim.SetInteger("animState", 1);
+        } else {
+            // Prevents flickering transitions to idle
+            delayToIdle -= Time.deltaTime;
+            if (delayToIdle < 0)
+                anim.SetInteger("animState", 0);
+        }
+
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+    }
     /// <summary>
     /// Initial initialization
     /// </summary>
@@ -33,6 +77,7 @@ public class Character : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         body2D = GetComponent<Rigidbody2D>();
+        
     }
 
     /// <summary>
@@ -41,59 +86,7 @@ public class Character : MonoBehaviour
     /// </summary>
     protected virtual void Update()
     {
-        // use Input.GetAxis for the X axis. The method returns the axis value in the range from -1 to 1.
-        // with standard project settings
-        // - 1 is returned when you press the left arrow (or the A key) on the keyboard,
-        // 1 is returned when pressing the right arrow (or the D key) on the keyboard
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-
-        // refer to the character component RigidBody2D. set its speed along the X axis,
-        // equal to the X axis value multiplied by the max. speed
-        body2D.velocity = new Vector2(moveX * maxSpeed, moveY * maxSpeed);
-        //body2D.velocity = new Vector2(moveX * maxSpeed, body2D.velocity.y);
-
-        // if you pressed the key to move to the right, and the character is directed to the left
-        if (moveX > 0 && !isFacingRight)
-            // flip the character to the right
-            Flip();
-        // reverse situation. flip the character to the left
-        else if (moveX < 0 && isFacingRight)
-            Flip();
-
-
-        // -- Handle Animations --
-
-        //Death
-        if (Input.GetKeyDown("e")) {
-            anim.SetTrigger("death");
-        }
-
-        //Hurt
-        else if (Input.GetKeyDown("q")) {
-            anim.SetTrigger("hurt");
-        }
-
-        //Attack
-        else if (Input.GetMouseButtonDown(0)) {
-            anim.SetTrigger("attack");
-        }
-
-        //Run
-        else if (Mathf.Abs(moveX) > Mathf.Epsilon || Mathf.Abs(moveY) > Mathf.Epsilon) {
-            // Reset timer
-            delayToIdle = 0.05f;
-            anim.SetInteger("animState", 1);
-        }
-
-        //Idle
-        else {
-            // Prevents flickering transitions to idle
-            delayToIdle -= Time.deltaTime;
-            if (delayToIdle < 0)
-                anim.SetInteger("animState", 0);
-        }
-
+        Move( controller.Player.Move.ReadValue<Vector2>());
     }
 
 
