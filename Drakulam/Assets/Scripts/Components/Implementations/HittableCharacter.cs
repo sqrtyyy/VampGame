df6d43330d39c43d2e5bfb3a6caf905f5e9b5b85
Aleksei <alex.zedfeelgood@gmail.com>
2021-03-14 19:15:52 +0300

    add atack

---
+++ Drakulam/Assets/Scripts/Components/Implementations/HittableCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HittableCharacter : IHittable
+{
+    public override void Hit(int damage)
+    {
+        GetComponent<IHealth>().changeHealth(-damage);
+        GetComponent<Animator>().SetTrigger("hurt");
+    }
+}
