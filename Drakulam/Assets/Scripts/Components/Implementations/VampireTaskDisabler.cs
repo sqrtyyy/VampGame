f69f4a3461454df5a826a22d362aac67cea56bdc
Viktor <viktor.gerasimenko.2014@mail.ru>
2021-03-23 19:49:25 +0300

    #118 create task with torches
    - animated torches
    - possibility to turn on/off torches
     -add taskManager script

---
+++ Drakulam/Assets/Scripts/Components/Implementations/VampireTaskDisabler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VampireTaskDisabler : ITaskCompleter
+{
+    public Transform center;
+
+    public LayerMask taskMask;
+
+    public float radius;
+
+    public override void FindTask()
+    {
+        Collider2D task = Physics2D.OverlapCircle(center.position, radius, taskMask);
+        if (task)
+        {
+            task.GetComponent<ITask>().SabotageTask();
+        }
+        else
+        {
+            Debug.Log("Tasks are not found");
+        }
+    }
+}
---
+++ Drakulam/Assets/Scripts/Tasks/TorchTask.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class TorchTask : ITask
+{
+
+    void Start()
+    {
+        AddTaskManager(TaskManager.Instance());
+
+        anim = GetComponent<Animator>();
+        light = gameObject.GetComponent<Light2D>();
+        ChangeStatus();
+    }
+
+    public override void StartTask()
+    {
+        if (!IsCompleted())
+        {
+            isOn = true;
+            ChangeStatus();
+        }
+    }
+
+    public override void SabotageTask()
+    {
+        if (IsCompleted())
+        {
+            isOn = false;
+            ChangeStatus();
+        }
+    }
+
+
+    public override string GetTaskName()
+    {
+        return "Зажгите факела";
+    }
+
+
+    public override bool IsCompleted()
+    {
+        return isOn;
+    }
+
+
+    private void ChangeStatus()
+    {
+        anim.SetBool("isOn", isOn);
+        light.enabled = isOn;
+        NotifyTaskManager(GetTaskName(), isOn);
+    }
+
+
+    private Light2D light;
+
+    public bool isOn;
+
+    private Animator anim;
+}
