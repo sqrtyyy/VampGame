246fb3482342dbfe9b340ac7fe50ffc8bd9a9381
MariLoNi <50141941+Nika-Keks@users.noreply.github.com>
2021-03-28 14:16:49 +0300

    #107 add ability to choose a room in multiplayer mod   - change the scroll of the overview of the list of rooms   - add join to the lobby   - add the ability to select rooms

--- Drakulam/Assets/Scripts/Multiplayer/LobbyManager.cs
+++ Drakulam/Assets/Scripts/Multiplayer/LobbyManager.cs
@@ -1,18 +1,22 @@
 ﻿using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    public List<RoomInfo> roomList;
+    public RoomListInitializer roomListInitializer;
+    //public RoomListInitializer roomListInitializer;
     // Start is called before the first frame update
     void Start()
     {
+        roomList = new List<RoomInfo>();
         PhotonNetwork.NickName = "Player " + Random.Range(1000, 9999);
         PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.GameVersion = "1.0.0";
         PhotonNetwork.ConnectUsingSettings();
-
     }
 
     // Update is called once per frame
@@ -24,6 +28,10 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log("Connected to server");
+        if (PhotonNetwork.JoinLobby())
+            Debug.Log("Join the lobby");
+        else
+            Debug.Log("!!! fatal join the lobby !!!"); 
         //base.OnConnectedToMaster();
     }
 
@@ -34,15 +42,27 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         //base.OnJoinedRoom();
     }
 
-    public void CreateRoom()
+    public void CreateRoom(string name, string password)
     {
+        foreach (var room in roomList)
+            if (name == room.Name)
+
         if (!PhotonNetwork.IsConnectedAndReady)
             return;
-        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 6 });
+        PhotonNetwork.CreateRoom(name, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
     }
 
-    public void JoinRoom()
+    public void JoinRoom(string name, string password)
     {
-        PhotonNetwork.JoinRandomRoom();
+        PhotonNetwork.JoinRoom(name);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log("Room list:");
+        this.roomList = roomList;
+        roomListInitializer.UpdateItems();
+        foreach (var room in roomList)
+            Debug.Log(room.Name);
     }
 }
