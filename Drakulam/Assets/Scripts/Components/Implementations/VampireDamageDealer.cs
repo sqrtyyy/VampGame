84d6b12d18839cf1569d691630103ff85648c169
Viktor <viktor.gerasimenko.2014@mail.ru>
2021-03-29 22:47:47 +0300

    #152 add necessary components to vampire

---
+++ Drakulam/Assets/Scripts/Components/Implementations/VampireDamageDealer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VampireDamageDealer : IDamageDealer
+{
+    public float timeBetweenAttacks;
+
+    public Transform attackPos;
+
+    public LayerMask EnemyMask;
+
+    public float attackRadius;
+
+    public int damage;
+
+    public Animator anim;
+
+    private float prevTime;
+
+
+    protected void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public override void Attack()
+    {
+        if (Time.time - prevTime > timeBetweenAttacks)
+        {
+            anim.SetTrigger("attack");
+            Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPos.position, attackRadius, EnemyMask);
+            foreach (var enemy in enemies)
+            {
+                enemy.GetComponent<IHittable>().Hit(damage);
+            }
+
+            prevTime = Time.time;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRadius);
+    }
+}
