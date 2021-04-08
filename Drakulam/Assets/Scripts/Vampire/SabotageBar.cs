df4de28c27bb4c0d44711eadfd54964870d9ab2f
Aleksei <alex.zedfeelgood@gmail.com>
2021-04-08 19:01:25 +0300

    #180 add sabotage icon

---
+++ Drakulam/Assets/Scripts/Vampire/SabotageBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SabotageBar : MonoBehaviour
+{
+    public Image bar;
+    private bool isInCycle = false;
+    IEnumerator checkTime()
+    {
+        isInCycle = true;
+        while (VampireTaskDisabler.getPercents() < 1)
+        {
+            
+            Debug.Log(VampireTaskDisabler.getPercents());
+            bar.fillAmount = VampireTaskDisabler.getPercents();
+            yield return new WaitForSeconds(1);
+        }
+
+        bar.fillAmount = 1;
+        isInCycle = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!isInCycle)
+        {
+            StartCoroutine(checkTime());
+        }
+    }
+    
+}
