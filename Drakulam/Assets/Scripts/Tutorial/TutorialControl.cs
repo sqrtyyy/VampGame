7c903dc495d4e17883e1a864dd446fe3588819a6
Дарья Смирнова <smirnova.ds@edu.spbstu.ru>
2021-04-08 19:10:45 +0300

    #192 Game tutorial
    - add graphics (canvases)
    - add input controller
    ________________
    tutorial control don't work

---
+++ Drakulam/Assets/Scripts/Tutorial/TutorialControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialControl : IControllable
+{
+    private TutoriaInput _tutorialManager;
+
+    private void Awake()
+    {
+        _tutorialManager = new TutoriaInput();
+        _tutorialManager.Tutorial.atack.performed += ctx => { GetComponent<TutorialManager>().AttackTrigger(); };
+        _tutorialManager.Tutorial.next.performed += ctx => { GetComponent<TutorialManager>().NextTrigger(); };
+        _tutorialManager.Tutorial.move.performed += ctx => { GetComponent<TutorialManager>().MoveTrigger(); };
+    }
+}
---
+++ Drakulam/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    public GameObject human;
+    public GameObject vampire;
+    public GameObject humanUI;
+    public GameObject vampireUI;
+    public ITask task;
+    public GameObject invisibleWall;
+    private HumanHealth _humanHealth;
+    private HumanHealth _vampireHealth;
+    private int popUpIndex;
+
+    private const int TutorialHMove = 0;
+    private const int TutorialHTaskList = 1;
+    private const int TutorialHTask = 2;
+    private const int TutorialHTimer = 3;
+    private const int TutorialHHealth = 4;
+    private const int TutorialHAttack = 5;
+
+    private const int TutorialVHumanDeath = 6;
+    private const int TutorialVTaskList = 7;
+    private const int TutorialVSabotage = 8;
+    private const int TutorialVSabotageTimer = 9;
+    private const int TutorialVMap = 10;
+    private const int TutorialVAttack = 11;
+
+
+    private void CompleteStep()
+    {
+        popUps[popUpIndex].SetActive(false);
+        popUpIndex++;
+        popUps[popUpIndex].SetActive(true);
+    }
+
+    private void InitializeHumanTutorial()
+    {
+        vampireUI.SetActive(false);
+        humanUI.SetActive(true);
+        human.GetComponent<Transform>().position = new Vector3(-94.22f, 26.3f, 0);
+        vampire.GetComponent<Transform>().position = new Vector3(-107.46f, -2.42f, -3.29f);
+        invisibleWall.SetActive(true);
+        human.GetComponent<CharacterControl>().gameObject.SetActive(true);
+        vampire.GetComponent<CharacterControl>().gameObject.SetActive(false);
+        popUps[0].SetActive(true);
+    }
+    
+    private void EndHumanTutorial()
+    {
+        _humanHealth.setHealth(10);
+        _vampireHealth.setHealth(100);
+    }
+    
+    private void InitializeVampireTutorial()
+    {
+        humanUI.SetActive(false);
+        vampireUI.SetActive(true);
+        vampire.GetComponent<Transform>().position = new Vector3(-94.22f, 26.3f, -3.29f);
+        human.GetComponent<Transform>().position = new Vector3(-107.46f, -2.42f, 0);
+        human.GetComponent<CharacterControl>().gameObject.SetActive(false);
+        vampire.GetComponent<CharacterControl>().gameObject.SetActive(true);
+    }
+    
+    private void EndVampireTutorial()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+    private void HumanDeathStep()
+    {
+        // move vampire or hit human
+    }
+
+    void Start()
+    {
+        InitializeHumanTutorial();
+        popUpIndex = 0;
+        _humanHealth = human.GetComponent<HumanHealth>();
+        _vampireHealth = vampire.GetComponent<HumanHealth>();
+        InitializeHumanTutorial();
+    }
+
+    void Update()
+    {
+        // human
+        if (popUpIndex == TutorialHTask && task.IsCompleted())
+            CompleteStep();
+        else if (popUpIndex == TutorialVHumanDeath)
+        { if(_humanHealth.getHealth() == 0)
+            {
+                EndHumanTutorial();
+                InitializeVampireTutorial();
+                CompleteStep();
+            }
+            else
+            {
+                HumanDeathStep();
+            }
+        }
+
+        // vampire
+        else if (popUpIndex == TutorialVSabotage && !task.IsCompleted())
+            CompleteStep();
+        else if (popUpIndex == TutorialVAttack && _humanHealth.getHealth() == 0)
+        {
+            EndVampireTutorial();
+            popUps[popUpIndex].SetActive(false);
+        }
+    }
+    
+    public void AttackTrigger()
+    {
+        Debug.Log("attackTrigger");
+        if (popUpIndex == TutorialHAttack)
+        {
+            CompleteStep();
+            invisibleWall.SetActive(false);
+        }
+    }
+
+    public void NextTrigger()
+    {
+        Debug.Log("nextTrigger");
+        if (popUpIndex == TutorialHTaskList || 
+            popUpIndex == TutorialHTimer || 
+            popUpIndex == TutorialHHealth ||
+            popUpIndex == TutorialVSabotageTimer ||
+            popUpIndex == TutorialVTaskList||
+            popUpIndex == TutorialVMap)
+            CompleteStep();
+    }
+
+    public void MoveTrigger()
+    {
+        Debug.Log("moveTrigger");
+        if (popUpIndex == TutorialHMove)
+            CompleteStep();
+    }
+    
+}
