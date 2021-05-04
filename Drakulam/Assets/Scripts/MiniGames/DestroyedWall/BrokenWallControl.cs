c8c392d6708283a7285671ef8e57251ec2ebeab7
Viktor <viktor.gerasimenko.2014@mail.ru>
2021-05-04 18:39:20 +0300

    #293 first version of description

---
+++ Drakulam/Assets/Scripts/MiniGames/DestroyedWall/BrokenWallControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenWallControl : IControllable
+{
+    private BrokenWallInput _brickWallManager;
+    public TaskDestroyedWall manager;
+
+    private void Awake()
+    {
+        _brickWallManager = new BrokenWallInput();
+        _brickWallManager.BrokenWall.next.performed += ctx => { manager.NextTrigger(); };
+   
+    }
+
+    private void OnEnable()
+    {
+        _brickWallManager.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _brickWallManager.Disable();
+    }
+}
---
+++ Drakulam/Assets/Scripts/MiniGames/DestroyedWall/BrokenWallInput.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Actions/BrokenWallActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @BrokenWallInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @BrokenWallInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""BrokenWallActions"",
+    ""maps"": [
+        {
+            ""name"": ""BrokenWall"",
+            ""id"": ""ee78ecfb-3d23-492e-9ed5-f6536dc3195f"",
+            ""actions"": [
+                {
+                    ""name"": ""next"",
+                    ""type"": ""Button"",
+                    ""id"": ""64a86db2-fc66-431f-a8e1-4770557efe23"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""853bfdb2-5860-4aec-a103-d6edce6b7c89"",
+                    ""path"": ""<Keyboard>/enter"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""next"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // BrokenWall
+        m_BrokenWall = asset.FindActionMap("BrokenWall", throwIfNotFound: true);
+        m_BrokenWall_next = m_BrokenWall.FindAction("next", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // BrokenWall
+    private readonly InputActionMap m_BrokenWall;
+    private IBrokenWallActions m_BrokenWallActionsCallbackInterface;
+    private readonly InputAction m_BrokenWall_next;
+    public struct BrokenWallActions
+    {
+        private @BrokenWallInput m_Wrapper;
+        public BrokenWallActions(@BrokenWallInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @next => m_Wrapper.m_BrokenWall_next;
+        public InputActionMap Get() { return m_Wrapper.m_BrokenWall; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(BrokenWallActions set) { return set.Get(); }
+        public void SetCallbacks(IBrokenWallActions instance)
+        {
+            if (m_Wrapper.m_BrokenWallActionsCallbackInterface != null)
+            {
+                @next.started -= m_Wrapper.m_BrokenWallActionsCallbackInterface.OnNext;
+                @next.performed -= m_Wrapper.m_BrokenWallActionsCallbackInterface.OnNext;
+                @next.canceled -= m_Wrapper.m_BrokenWallActionsCallbackInterface.OnNext;
+            }
+            m_Wrapper.m_BrokenWallActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @next.started += instance.OnNext;
+                @next.performed += instance.OnNext;
+                @next.canceled += instance.OnNext;
+            }
+        }
+    }
+    public BrokenWallActions @BrokenWall => new BrokenWallActions(this);
+    public interface IBrokenWallActions
+    {
+        void OnNext(InputAction.CallbackContext context);
+    }
+}
