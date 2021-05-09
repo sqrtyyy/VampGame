5cc22462bf2510df97ff079b1c453b9156467d79
Nika-Keks <as79020947761@gmail.com>
2021-05-09 16:07:32 +0300

    #282 Fix bug with room displaying in room list
     - add a message about connecting to the server to the menu
     - set a fixed region for connecting ru

---
+++ Drakulam/Assets/Scripts/Multiplayer/ConStatusController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ConStatusController : MonoBehaviour
+{
+    [SerializeField] Button retryBtn;
+    [SerializeField] GameObject connectionFrame;
+    [SerializeField] GameObject menuFrame;
+
+    public void ShowBtn(Action callBack)
+    {
+        retryBtn.gameObject.SetActive(true);
+
+        retryBtn.onClick.AddListener(() =>
+        {
+            callBack();
+            retryBtn.onClick.RemoveAllListeners();
+            retryBtn.gameObject.SetActive(false);
+        });
+    }
+
+    public void FinishConnectin()
+    {
+        connectionFrame.SetActive(false);
+        menuFrame.SetActive(true);
+    }
+
+    public void StartConnectin()
+    {
+        connectionFrame.SetActive(true);
+        menuFrame.SetActive(false);
+    }
+}
