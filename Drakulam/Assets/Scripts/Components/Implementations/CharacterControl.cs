21bf168992b1625c0f0e396d463522eaaa4513ad
Nika-Keks <as79020947761@gmail.com>
2021-05-09 22:33:08 +0300

    #289 Inability to leave active game
     - remove the ability to exit the game after it has started
     - fix bug with restarting the game

--- Drakulam/Assets/Scripts/Components/Implementations/CharacterControl.cs
+++ Drakulam/Assets/Scripts/Components/Implementations/CharacterControl.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using Photon.Pun;
 
 public class CharacterControl : IControllable
@@ -44,6 +41,9 @@ public class CharacterControl : IControllable
         {
             if (photonView.IsMine)
             {
+                if (GameManager.GameStarted())
+                    return;  
+                PhotonNetwork.LeaveRoom();
                 SceneManager.LoadScene("menu");
             }
         };
